1dee59d60e0ee2a3963787a5e923e579448c918e
SubhamAdhikary2000 <parimaladhikary360@gmail.com>
2024-09-30 18:17:54 +0530

    Update q4.cpp

--- q4.cpp
+++ q4.cpp
@@ -1,10 +1,24 @@
-/*No the declaration "void func(char= '*', int =45);" is not valid in c++
+/*
+The line void func(char='*', int=45); is not a valid declaration of a function in C++
+due to the placement of the default argument values. Default arguments must be specified
+in the function declaration, not the function prototype.In C++, you can provide default
+values for function parameters, but they must be defined in the function declaration
+(prototype) or the function definition, not both.If you declare it as void func(char='*', int=45);
+you will get a compilation error because the compiler does not expect default values in a function prototype.
+*/
+#include <iostream>
+using namespace std;
+//Function declaration with default parameters
+void func(char c = '*', int num = 45);
+//Function definition
+void func(char c, int num)
+{
+    cout << "Character: " << c << ", Number: " << num << endl;
+}
 
-in C++,when defining function paramter with default values, you must specify the 
-data type for each paramter. The correct way to declare this fuction would be:*/
-
-void func(char ch= '*', int=45);
-
-/*If we declare this way the compiler wil generate a syntex error. The compiler will complain
-that declaration is not valid because it expects a type before the parameter name. we will see
-an error message indicating a "type name is not allowed"*/:
+int main() {
+    func();        // Uses default values
+    func('#');    // Uses '#' and default for num
+    func('!', 100); // Uses '!' and 100
+    return 0;
+}
